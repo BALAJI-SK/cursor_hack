68338bddbcaeced57b6a440dfa7a4fffbe69f90e
S K BALAJI <skbalajimbl1@gmail.com>
2026-07-05 12:05:10 +0100

    fix(backend): synchronize TFLite interpreter and copy output tensor to prevent internal reference crashes

--- server/model_runner.py
+++ server/model_runner.py
@@ -1,6 +1,7 @@
 import numpy as np
 from PIL import Image
 import tensorflow.lite as tflite
+import threading
 
 class ModelRunner:
     def __init__(self, model_path: str):
@@ -8,6 +9,7 @@ class ModelRunner:
         self.interpreter.allocate_tensors()
         self.input_details = self.interpreter.get_input_details()
         self.output_details = self.interpreter.get_output_details()
+        self.lock = threading.Lock()
 
     def run_inference(self, image: Image.Image) -> tuple[np.ndarray, dict]:
         pageW, pageH = image.size
@@ -26,9 +28,10 @@ class ModelRunner:
         img_data = np.array(padded, dtype=np.float32) / 255.0
         img_data = np.expand_dims(img_data, axis=0)
 
-        self.interpreter.set_tensor(self.input_details[0]['index'], img_data)
-        self.interpreter.invoke()
-        raw_output = self.interpreter.get_tensor(self.output_details[0]['index'])
+        with self.lock:
+            self.interpreter.set_tensor(self.input_details[0]['index'], img_data)
+            self.interpreter.invoke()
+            raw_output = self.interpreter.get_tensor(self.output_details[0]['index']).copy()
 
         return raw_output, {
             "scale": scale,
